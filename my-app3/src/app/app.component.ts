399cf727746d7ec100cea31678cc28aaba479ecf
vishal kalaskar <83940132+vishalkalaskar@users.noreply.github.com>
2025-04-05 17:38:45 +0000

    thrid

--- my-app3/src/app/app.component.ts
+++ my-app3/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component,OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {UserserviceService} from './services/userservice.service';
 import { CommonModule } from '@angular/common'; // ✅ Import this
+import { EmailValidator } from '@angular/forms';
+import { validateHeaderName } from 'http';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,CommonModule],
@@ -24,4 +26,48 @@ export class AppComponent implements OnInit {
      // console.log(this.User);
     });
   }
+
+  //click Event
+  clickEvent(event:any)
+  {
+      console.log("function call",event.type);
+      console.log("function called",(event.target as Element).classList);  //className;
+      console.log("function ",event.target);
+  }
+
+  inputEvent(event: Event): void {
+  const target = event.target as HTMLInputElement;
+  console.log("Input value:", target.value);
+}
+
+vari="";
+getVariable(even:Event)
+{ 
+   const value = even.target as HTMLInputElement;
+   console.log("get variable",value.value);
+   this.vari=value.value;
+  
+}
+ displayname="";
+
+ getvalueb()
+ {
+   this.displayname=this.vari;
+ }
+
+ setName()
+ {
+   this.vari="sam";
+ }
+
+ mail="";
+ getEmail(val:string)
+ {
+        console.log(val);
+        this.mail=val;
+ }
+ setMail() {
+  this.mail="defaul@gmail.com";
+  console.log("Email set to:", this.mail);
+}
 }
